eec5c20c003902bbc2477d45ccf0e28eaf5731ef
EunahPae <eunahp86@gmail.com>
2024-07-17 11:20:01 +0900

    0717

---
+++ MybatisProject/src/test/java/com/springbook/biz/board/BoardServiceClientTset.java
@@ -0,0 +1,25 @@
+package com.springbook.biz.board;
+
+import java.util.List;
+
+import com.springbook.biz.board.impl.BoardDAO;
+
+public class BoardServiceClientTset {
+	public static void main(String[] args) {
+		BoardDAO boardDAO = new BoardDAO();
+
+		BoardVO vo = new BoardVO();
+		vo.setTitle("mybatis 제목");
+		vo.setWriter("홍길동");
+		vo.setContent("mybatis 내용입니다......");
+		boardDAO.insertBoard(vo);
+
+		vo.setSearchCondition("TITLE");
+		vo.setSearchKeyword("");
+		List<BoardVO> boardList = boardDAO.gettBoardList(vo);
+		for (BoardVO board : boardList) {
+			System.out.println("-----> " + board.toString());
+		}
+
+	}
+}
