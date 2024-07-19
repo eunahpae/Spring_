7b1176039e6e9a1aee6046f8040ed09c817f4f89
EunahPae <eunahp86@gmail.com>
2024-07-19 15:53:45 +0900

    주석깨짐 해결

--- Board/src/main/java/com/lbi/mapper/BoardMapper.java
+++ Board/src/main/java/com/lbi/mapper/BoardMapper.java
@@ -7,16 +7,22 @@ import com.lbi.model.Criteria;
 
 public interface BoardMapper {
 	
+	// 게시물 등록
 	public void enroll(BoardVO board);
 
+	// 게시물 조회
 	public List<BoardVO> getList();
 	
+	// 게시물 상세 조회
 	public BoardVO getPage(int bno);
 
+	// 게시물 수정
 	public int modify(BoardVO board);
 
+	// 게시물 삭제
 	public int delete(int bno);
 
+	// 게시물 목록(페이징 적용)
 	public List<BoardVO> getListPaging(Criteria cri);
 
 	// 게시물 총 개수
